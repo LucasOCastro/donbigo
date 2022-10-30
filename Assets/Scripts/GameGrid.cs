44d53ae849ba8e8b9370d204d4856dcfbaaee56a
lucas castro <lucas_o__castro@hotmail.com>
2022-10-30 18:32:26 -0300

    SPrites novos com rodape na parede

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace DonBigo
 {
@@ -47,6 +48,8 @@ namespace DonBigo
 
         public GameGrid(int size, Grid unityGrid)
         {
+            Tilemap tm;
+            tm.get
             Size = size;
             _unityGrid = unityGrid;
             _tiles = new Tile[size, size];
