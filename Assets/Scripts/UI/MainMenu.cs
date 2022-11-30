b1976b809b530b330cd5294511977d11f3ba133c
lucas castro <lucas_o__castro@hotmail.com>
2022-11-30 15:54:25 -0300

    Implementa o botão de créditos

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DonBigo.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button muteButton;
+        [SerializeField] private Button creditsButton;
+        [SerializeField] private Image creditsMenu;
+        
+        private void Awake()
+        {
+            playButton.onClick.AddListener(() => Debug.Log("play"));
+            muteButton.onClick.AddListener(() => Debug.Log("mute"));
+            creditsButton.onClick.AddListener(() => SetCredits(true));
+        }
+
+        private void SetCredits(bool open)
+        {
+            creditsMenu.gameObject.SetActive(open);
+            if (open)
+            {
+                StartCoroutine(CreditsCoroutine());
+            }
+        }
+
+        private IEnumerator CreditsCoroutine()
+        {
+            while (creditsMenu.isActiveAndEnabled)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                {
+                    SetCredits(false);
+                    break;
+                }
+                yield return null;
+            }
+        }
+    }
+}
