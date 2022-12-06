4a5bf66d02ccaf8b8216fead2db416807613363b
lucas castro <lucas_o__castro@hotmail.com>
2022-12-05 23:03:37 -0300

    Estrutura básica de turnos e ações

---
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DonBigo
+{
+    /// <summary>
+    /// Ação responsável por mover uma entidade em 1 tile.
+    /// </summary>
+    public class MoveAction : Action
+    {
+        private Tile _target;
+        public MoveAction(Entity doer, Tile target) : base(doer)
+        {
+            if (target == null)
+            {
+                Debug.LogError("Target nulo em MoveAction!");
+            }
+            _target = target;
+        }
+
+        public override void Execute()
+        {
+            Doer.Tile = _target;
+        }
+    }
+}
---
+++ Assets/Scripts/Actions/TurnManager.cs
@@ -0,0 +1,32 @@
+﻿namespace DonBigo
+{
+    public class TurnManager
+    {
+        private Entity[] _entities;
+        private int _entityIndex;
+        public Entity CurrentEntity => _entities[_entityIndex];
+
+        public TurnManager(Entity[] entities)
+        {
+            if (entities == null || entities.Length == 0)
+            {
+                UnityEngine.Debug.LogError("Nenhuma entidade entregue ao TurnManager!");
+                return;
+            }
+            _entities = entities;
+        }
+
+        private void CycleEntity() => _entityIndex = (_entityIndex + 1) % _entities.Length;
+
+        public void Update()
+        {
+            //TODO as entidades devem ter um método que espera a seleção de uma action a ser realizada.
+            Action action = new MoveAction(CurrentEntity, CurrentEntity.Tile); //CurrentEntity.GetAction();
+            if (action != null)
+            {
+                action.Execute();
+                CycleEntity();
+            }
+        }
+    }
+}
