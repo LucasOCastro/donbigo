d8a035f56a8dac3fbc6fabd4fe92eecddd6a29f7
lucas castro <lucas_o__castro@hotmail.com>
2022-12-07 13:41:36 -0300

    Conserta as prateleiras quanticas

--- Assets/Scripts/Tiles/RandomStructureTileType.cs
+++ Assets/Scripts/Tiles/RandomStructureTileType.cs
@@ -7,12 +7,21 @@ namespace DonBigo
     {
         [SerializeField] private Sprite[] randomSprites;
 
+        private static int RandIndex(Vector3Int pos, int mod)
+        {
+            int val = (pos.x * pos.y) % 17 + 
+                      (pos.y * pos.y * pos.x) % 2 + 
+                      (pos.x * pos.x * pos.y) % 5 +
+                      (pos.z) % 13;
+            return val % mod;
+        }
+        
         public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
         {
             base.GetTileData(position, tilemap, ref tileData);
             if (randomSprites != null && randomSprites.Length > 0)
             {
-                tileData.sprite = randomSprites.Random();
+                tileData.sprite = randomSprites[RandIndex(position, randomSprites.Length)];
             }
         }
     }
