758123ef22f7855cefc92c15d5a850e8badcfd82
jpbuzzo <jpedrobuzzo@gmail.com>
2022-12-11 16:58:15 -0300

    Donbigo agora spawna na sala central

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DonBigo;
 
 namespace DonBigo
 {
@@ -10,16 +11,18 @@ namespace DonBigo
         [SerializeField] private Sprite[] donbigoSprite;
         public GameObject phantonette;
         [SerializeField] private Sprite[] phantonetteSprite;
-
+        // private GameGrid grid;
         private void Start()
         {
+            Spawner spawner = new Spawner();
             // Geração do Don Bigo
             donbigo = new GameObject("Player", typeof(SpriteRenderer), typeof(Bigodon));
             SpriteRenderer DBRenderer = donbigo.GetComponent<SpriteRenderer>();
             DBRenderer.sprite = donbigoSprite[7];
             donbigo.tag = "Player";
             // Abaixo setar a posição quando tiver um Spawn implementado vvvvvvvvvvvvv
-            donbigo.GetComponent<Entity>().Walk(GridManager.Instance.Grid.WorldToTile(new Vector2(0,0)));
+            // donbigo.GetComponent<Entity>().Walk(GridManager.Instance.Grid.WorldToTile(new Vector2(0,0)));
+            spawner.Spawn(GridManager.Instance.Grid, donbigo.GetComponent<Entity>());
             
             // Geração da Phantonette
             phantonette = new GameObject("Foe", typeof(SpriteRenderer), typeof(Phantonette));
