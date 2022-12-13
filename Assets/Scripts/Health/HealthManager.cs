d3447424afe1a12283aa53d20be7ed221293a800
lucas castro <lucas_o__castro@hotmail.com>
2022-12-13 13:41:12 -0300

    Health manager basico

---
+++ Assets/Scripts/Health/HealthManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DonBigo.Actions;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+namespace DonBigo
+{
+    public class HealthManager
+    {
+        public Entity Owner { get; }
+
+        private readonly List<HealthStatus> _statusList = new List<HealthStatus>();
+        public HealthManager(Entity owner)
+        {
+            Owner = owner;
+        }
+        
+        /// <summary>
+        /// Ticka todos os status e tenta gerar uma ação mais prioritária dentre eles.
+        /// </summary>
+        public Action Tick()
+        {
+            for (var i = 0; i < _statusList.Count; i++)
+            {
+                var status = _statusList[i];
+                bool shouldEnd = status.Tick(this);
+                if (shouldEnd)
+                {
+                    status.End(this);
+                    _statusList.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            foreach (var status in _statusList)
+            {
+                var action = status.GenAction(this);
+                if (action != null)
+                {
+                    return action;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Health/HealthStatus.cs
@@ -0,0 +1,20 @@
+﻿using DonBigo.Actions;
+
+namespace DonBigo
+{
+    public abstract class HealthStatus
+    {
+        /// <summary> Chamado pelo HealthManager quando o estado começa. </summary>
+        public abstract void Start(HealthManager health);
+        
+        /// <summary> Chamado pelo HealthManager quando o estado termina. </summary>
+        public abstract void End(HealthManager health);
+        
+        /// <summary> Chamado pelo HealthManager a cada turno. </summary>
+        /// <returns>Retorna true se o status deve acabar. False se deve continuar.</returns>
+        public abstract bool Tick(HealthManager health);
+        
+        /// <summary> Chamado pelo HealthManager após o tick, gera uma ação a ser executada pela entidade. </summary>
+        public abstract Action GenAction(HealthManager health);
+    }
+}
