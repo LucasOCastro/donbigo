f4074b4495d3bc1d9d8d2e22f8790790b74d694a
lucas castro <lucas_o__castro@hotmail.com>
2022-12-13 13:41:26 -0300

    Status de stun

---
+++ Assets/Scripts/Health/StunStatus.cs
@@ -0,0 +1,24 @@
+﻿using DonBigo.Actions;
+
+namespace DonBigo
+{
+    public class StunStatus : TurnLimitHealthStatus
+    {
+        public StunStatus(int stunTurns) : base(stunTurns)
+        {
+        }
+        
+        public override void Start(HealthManager health)
+        {
+        }
+
+        public override void End(HealthManager health)
+        {
+        }
+
+        public override Action GenAction(HealthManager health)
+        {
+            return new IdleAction(health.Owner);
+        }
+    }
+}
