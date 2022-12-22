050fb795fca8227b5c104180fc2b7ac0a94f9f6a
lucas castro <lucas_o__castro@hotmail.com>
2022-12-22 12:40:46 -0300

    Objetivos de ataque e fuga usando o GoToTargetObjective.cs refatorado

---
+++ Assets/Scripts/AI/Objectives/AttackEntityObjective.cs
@@ -0,0 +1,30 @@
+﻿using DonBigo.Actions;
+using UnityEngine;
+
+namespace DonBigo.AI
+{
+    public class AttackEntityObjective : GoToTargetObjective
+    {
+        private HealthManager _targetHealth;
+        public AttackEntityObjective(Entity doer, Entity target) : base(doer, target)
+        {
+            _targetHealth = target.Health;
+        }
+
+        public override Action Tick()
+        {
+            //TODO Quando tivermos os novos itens, isso aqui deverá ser adaptado para diferentes tipos de "armas".
+            //Melee: só ataca quando chega no player
+            //Ranged: tenta atacar da distância que der
+            //Trap: tenta colocar no caminho do player, ou entre si e o player (se tiver ranged)
+            //Também tem que considerar usar uma netgun antes de dar tiro, por exemplo
+
+            if (Doer.Tile.Pos.AdjacentTo(_target.Tile.Pos))
+            {
+                Debug.Log("TOME de " + Doer + " para " + _target);
+                return null;
+            }
+            return base.Tick();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Objectives/FleeObjective.cs
@@ -0,0 +1,58 @@
+﻿using DonBigo.Actions;
+using DonBigo.Rooms;
+
+namespace DonBigo.AI
+{
+    public class FleeObjective : GoToTargetObjective
+    {
+        private ITileGiver _fleeFrom;
+        private RoomExit? _targetExit;
+        private Path _targetPath;
+        public FleeObjective(Entity doer, ITileGiver target) : base(doer, null)
+        {
+            _fleeFrom = target;
+        }
+
+        private RoomExit? GetBestExit()
+        {
+            var currentRoom = Doer.Tile.ParentGrid.RoomAt(Doer.Tile.Pos);
+
+            RoomExit? bestExit = null;
+            float bestScore = 0;
+            foreach (var exit in currentRoom.Doors)
+            {
+                var distanceToDoer = Doer.Tile.Pos.ManhattanDistance(exit.Position);
+                var distanceToTarget = _fleeFrom.Tile.Pos.ManhattanDistance(exit.Position);
+                float score = (1f / distanceToDoer) - distanceToTarget;
+                if (bestExit == null || score > bestScore)
+                {
+                    bestExit = exit;
+                    bestScore = score;
+                }
+            }
+
+            return bestExit;
+        }
+            
+        public override Action Tick()
+        {
+            if (_targetExit != null && _targetExit.Value.Position.AdjacentTo(Doer.Tile.Pos))
+            {
+                return new UseDoorAction(Doer, _targetExit.Value);
+            }
+                
+            RoomExit? bestExit = GetBestExit();
+            if (_targetExit == null && bestExit == null)
+            {
+                return null;
+            }
+                
+            if (bestExit != null && (_targetExit == null || _targetExit.Value.Position != bestExit.Value.Position))
+            {
+                _targetExit = bestExit;
+                //objective = new FollowObjective(entity, entity.Tile.ParentGrid[bestExit.Value.Position]);
+            }
+            return base.Tick();
+        }
+    }
+}
