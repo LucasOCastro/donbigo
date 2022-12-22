c8cf33cdf9cd3d17ed1062483840c441090215ee
lucas castro <lucas_o__castro@hotmail.com>
2022-12-22 12:47:59 -0300

    Protótipo de AlertedState

---
+++ Assets/Scripts/AI/States/AlertedState.cs
@@ -0,0 +1,27 @@
+﻿using DonBigo.Actions;
+
+namespace DonBigo.AI
+{
+    public class AlertedState : AIState
+    {
+        protected override AIState OnTick(Entity entity, out AIObjective objective)
+        {
+            //TODO Desse jeito, ele para de fugir/perseguir quando perde o inimigo de vista, o que é bem ineficaz.
+            if (entity.SeesPlayer)
+            {
+                var player = CharacterManager.DonBigo;
+                //TODO não é efetivo retornar um novo objetivo todo tick, tenho que criar 1 e só criar outro quando precisar
+                bool stronger = entity.Inventory.CombatPower >= player.Inventory.CombatPower;
+                objective = stronger
+                    ? new AttackEntityObjective(entity, player)
+                    : new FleeObjective(entity, player);
+                return null;
+            }
+
+            objective = null;
+            return new WanderState();
+        }
+
+        
+    }
+}
