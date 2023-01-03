1f709a9eb5a2af2785aebf39f248d448d25020c3
lucas castro <lucas_o__castro@hotmail.com>
2023-01-03 12:14:36 -0300

    Implementa animações no menu principal

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -10,43 +10,56 @@ namespace DonBigo.UI
         [SerializeField] private Button playButton;
         [SerializeField] private Button muteButton;
         [SerializeField] private Button creditsButton;
-        [SerializeField] private Image creditsMenu;
 
         [SerializeField] private int playSceneID;
-        
+
+        [SerializeField] private string playAnimationName;
+        [SerializeField] private string creditsOpenAnimationName;
+        [SerializeField] private string creditsCloseAnimationName;
+
+        private Animator _animator;
         private void Awake()
         {
-            playButton.onClick.AddListener(PlayGame);
+            _animator = GetComponent<Animator>();
+            playButton.onClick.AddListener(() => StartCoroutine(PlayCoroutine()));
             muteButton.onClick.AddListener(() => Debug.Log("mute"));
-            creditsButton.onClick.AddListener(() => SetCredits(true));
+            creditsButton.onClick.AddListener((() => StartCoroutine(CreditsCoroutine())));
         }
 
-        private void PlayGame()
+        private bool AnimationPlaying(string anim)
         {
+            var state = _animator.GetCurrentAnimatorStateInfo(0);
+            return state.IsName(anim) && state.normalizedTime < 1;
+        }
+
+        private void SetButtons(bool active)
+        {
+            playButton.gameObject.SetActive(active);
+            muteButton.gameObject.SetActive(active);
+            creditsButton.gameObject.SetActive(active);
+        }
+
+        private IEnumerator PlayAndAwaitAnimationCoroutine(string anim)
+        {
+            _animator.Play(anim);
+            yield return null;
+            yield return new WaitWhile(() => AnimationPlaying(anim));
+        }
+        
+        private IEnumerator PlayCoroutine()
+        {
+            SetButtons(false);
+            yield return PlayAndAwaitAnimationCoroutine(playAnimationName);
             SceneManager.LoadScene(playSceneID);
-        }
-
-        private void SetCredits(bool open)
-        {
-            creditsMenu.gameObject.SetActive(open);
-            StopAllCoroutines();
-            if (open)
-            {
-                StartCoroutine(CreditsCoroutine());
-            }
         }
 
         private IEnumerator CreditsCoroutine()
         {
-            while (creditsMenu.isActiveAndEnabled)
-            {
-                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
-                {
-                    SetCredits(false);
-                    break;
-                }
-                yield return null;
-            }
+            SetButtons(false);
+            yield return PlayAndAwaitAnimationCoroutine(creditsOpenAnimationName);
+            yield return new WaitUntil(() => Input.anyKeyDown);
+            yield return PlayAndAwaitAnimationCoroutine(creditsCloseAnimationName);
+            SetButtons(true);
         }
     }
 }
