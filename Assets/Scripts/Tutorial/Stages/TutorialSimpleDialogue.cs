28bf0124eb98e32ea8e14abff58065cd6238572b
lucas castro <lucas_o__castro@hotmail.com>
2023-03-01 19:57:19 -0300

    Conserta o inspetor dos diálogos e adiciona a primeira sequencia.

--- Assets/Scripts/Tutorial/Stages/TutorialSimpleDialogue.cs
+++ Assets/Scripts/Tutorial/Stages/TutorialSimpleDialogue.cs
@@ -6,7 +6,11 @@ namespace DonBigo.Tutorial.Stages
     [CreateAssetMenu(fileName = "DialogueTutorialStage", menuName = "Tutorial/Dialogue")]
     public class TutorialSimpleDialogue : TutorialStage
     {
+        [TextArea(2,5)]
         [SerializeField] private string[] dialogues;
+
+        [TextArea(2,5)]
+        [SerializeField] private string[] englishDialogues;
 
         public override IEnumerator UpdateCoroutine()
         {
