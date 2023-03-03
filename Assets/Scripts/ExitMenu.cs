75541b845b8e9782853d145c606c6a2e92ea52c6
lucas castro <lucas_o__castro@hotmail.com>
2023-03-03 17:57:43 -0300

    Protótipo do menu de pausa quando aperta esc

---
+++ Assets/Scripts/ExitMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DonBigo
+{
+    public class ExitMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject menu;
+        [SerializeField] private Button exitButton;
+        [SerializeField] private Button closeButton;
+        [SerializeField] private KeyCode menuKey = KeyCode.Escape;
+
+        public bool Open
+        {
+            get => menu.activeSelf;
+            set => menu.SetActive(value);
+        }
+
+        private void Awake()
+        {
+            exitButton.onClick.AddListener(() => GameEnder.Instance.EndGame(GameEnder.Condition.Exit));
+            closeButton.onClick.AddListener(() => Open = false);
+            Open = false;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(menuKey))
+            {
+                Open = !Open;
+            }
+        }
+    }
+}
