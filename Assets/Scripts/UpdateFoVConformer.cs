485ab0133f1ae30c1f3f195e00209cec1d798df9
lucas castro <lucas_o__castro@hotmail.com>
2023-03-04 00:51:02 -0300

    Efeitos de stun somem quando fora do FoV

---
+++ Assets/Scripts/UpdateFoVConformer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DonBigo
+{
+    //O certo seria conectar isso a um ITileGiver mas taok
+    public class UpdateFoVConformer : MonoBehaviour
+    {
+        private SpriteRenderer _renderer;
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            Vector2Int tile = GridManager.Instance.Grid.WorldToTilePos(transform.position);
+            _renderer.enabled = FieldOfViewRenderer.IsVisible(tile);
+        }
+    }
+}
