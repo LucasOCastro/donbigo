4889ee5ede4140ff78ce237489bd857457b494c3
lucas castro <lucas_o__castro@hotmail.com>
2023-03-16 09:34:46 -0300

    Faz menus fecharem quando clica fora deles

---
+++ Assets/Scripts/UI/HideOnContextLoss.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DonBigo.UI
+{
+    public class HideOnContextLoss : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private bool closeOnKeyPress;
+        private bool _inContext;
+
+        private bool ShouldClose() =>
+            (Input.GetMouseButtonDown(0) && !_inContext) ||
+            (closeOnKeyPress && Input.anyKeyDown && !Input.GetMouseButtonDown(0));
+            
+        private void Update()
+        {
+            if (ShouldClose())
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _inContext = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _inContext = false;
+        }
+    }
+}
