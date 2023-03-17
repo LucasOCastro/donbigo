01e4775313933fbc0066bd800dc925934e057679
lucas castro <lucas_o__castro@hotmail.com>
2023-03-17 10:32:04 -0300

    Muda os diálogos para ficarem salvos em json

---
+++ Assets/Scripts/Tutorial/LocalizedDialogueAsset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DonBigo.Tutorial
+{
+    [Serializable]
+    public class LocalizedDialogueAsset
+    {
+        [SerializeField] private TextAsset asset;
+
+        public void OnValidate()
+        {
+            if (asset != null) JsonUtility.FromJsonOverwrite(asset.text, this);
+        }
+        
+        [TextArea(2,5)]
+        public string[] br;
+        
+        [TextArea(2,5)]
+        public string[] en;
+
+        public string[] Get(bool english) => english ? en : br;
+    }
+}
--- Assets/Scripts/Tutorial/Stages/TutorialSimpleDialogue.cs
+++ Assets/Scripts/Tutorial/Stages/TutorialSimpleDialogue.cs
@@ -6,15 +6,32 @@ namespace DonBigo.Tutorial.Stages
     [CreateAssetMenu(fileName = "DialogueTutorialStage", menuName = "Tutorial/Dialogue")]
     public class TutorialSimpleDialogue : TutorialStage
     {
-        [TextArea(2,5)]
-        [SerializeField] private string[] dialogues;
+        /*[MenuItem("Assets/Gen Json Assets")]
+        public static void GenJsonAssets()
+        {
+            foreach (var obj in Selection.objects)
+            {
+                if (obj is not TutorialSimpleDialogue dialogue) continue;
+                
+                string text = JsonUtility.ToJson(dialogue, prettyPrint: true)
+                    .Replace(nameof(dialogue.dialogues), "br")
+                    .Replace(nameof(dialogue.englishDialogues), "en");
 
-        [TextArea(2,5)]
-        [SerializeField] private string[] englishDialogues;
+                string assetPath = AssetDatabase.GetAssetPath(dialogue)
+                    .Replace("Assets", "")
+                    .Replace(".asset", ".json");
+                File.WriteAllText(Application.dataPath + assetPath, text);
+            }
+            AssetDatabase.Refresh();
+        }*/
+
+        [SerializeField] private LocalizedDialogueAsset dialogueAsset;
+        
+        private void OnValidate() => dialogueAsset?.OnValidate();
 
         public override IEnumerator UpdateCoroutine()
         {
-            yield return WaitForDialogue(Settings.English ? englishDialogues : dialogues);
+            yield return WaitForDialogue(dialogueAsset.Get(Settings.English));
         }
     }
 }
