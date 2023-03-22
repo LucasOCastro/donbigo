4f91b672636a12e004a785a45cbdfbbcf9821c94
lucas castro <lucas_o__castro@hotmail.com>
2023-03-22 18:43:02 -0300

    Desativa o FoV no tutorial

--- Assets/Scripts/UpdateFoVConformer.cs
+++ Assets/Scripts/UpdateFoVConformer.cs
@@ -22,6 +22,8 @@ namespace DonBigo
         
         private void Update()
         {
+            if (!FieldOfViewRenderer.DEBUG_drawVis) return;
+        
             if (_renderer)
                 _renderer.enabled = ShouldBeEnabled();
         }
